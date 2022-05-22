7f3abb7f09401fdfa8cb0a27969f088832a7710c
Suronjit pal <suronjit797@gmail.com>
2022-05-22 18:21:22 +0600

    side nav

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './Dashboard.css'
-import { Link, Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 const Dashboard = () => {
 
     return (
         <div>
             <div className='dashboard'>
-                <div className="dashboard-left">
-                    <Link to='/dashboard'> My Orders </Link>
-                    <Link to='/dashboard/add-review'> Add A Review </Link>
-                    <Link to='/dashboard/my-profile'> My Profile </Link>
-                    <Link to='/dashboard/manage-all-orders'> Manage All Orders </Link>
-                    <Link to='/dashboard/add-product'> Add A Product </Link>
-                    <Link to='/dashboard/manage-products'> Manage Products </Link>
+                <div className="dashboard-left bg-dark">
+                    <NavLink to='/dashboard/'> My Orders </NavLink>
+                    <NavLink to='/dashboard/add-review'> Add A Review </NavLink>
+                    <NavLink to='/dashboard/my-profile'> My Profile </NavLink>
+                    <NavLink to='/dashboard/manage-all-orders'> Manage All Orders </NavLink>
+                    <NavLink to='/dashboard/add-product'> Add A Product </NavLink>
+                    <NavLink to='/dashboard/manage-products'> Manage Products </NavLink>
                 </div>
                 <div className="dashboard-right">
                 <Outlet />
